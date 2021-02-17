abbc8890f40d9fa5e72c50371533ba189e7885d2
yuvalLevy1 <yuval124levy1@gmail.com>
2021-02-18 01:08:51 +0200

    added the getLastMessage() function that returns the last message from the chat

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-contact = "שלומקה"
+contact = "Laniel"
 text = "yeet"
 
 
@@ -34,12 +34,31 @@ def enter_chat(driver, contact):
     time.sleep(2)
 
 
+def get_last_message(driver):
+    messages = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/div[3]/div/div/div[3]")
+    in_messages = messages.find_elements_by_css_selector('div[class="_1RAno message-in focusable-list-item"]')
+    if len(in_messages) == 0:
+        print("couldn't find messages")
+        return
+    message = in_messages[-1]
+    message_elements = message.find_elements_by_tag_name('span')
+    for value in message_elements:
+        if value.get_attribute("class") == "_1Oy25":
+            return value.text
+
+
 def main():
     driver = initialize_firefox()
     enter_whatsapp(driver)
     enter_chat(driver, contact)
-    send_message(driver, text)
-    driver.quit()
+    last_message = ""
+    while True:
+        message = get_last_message(driver)
+        if message != last_message:
+            print(message)
+            last_message = message
+        time.sleep(0.1)
+    # driver.quit()
 
 
 if __name__ == '__main__':
